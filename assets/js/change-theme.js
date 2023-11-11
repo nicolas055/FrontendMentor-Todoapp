4d3f74c0b855508388f1ec451bc17327e230f471
Nicolas Leite <nicolasleite7@gmail.com>
2023-11-11 14:46:54 -0300

    Adding change theme functionality

---
+++ assets/js/change-theme.js
@@ -0,0 +1,22 @@
+// Getting elements from html
+const body = document.querySelector('body')
+const toggleTheme = document.querySelector('#toggle-theme');
+
+
+// Getting data from localStorage or prefers-color-scheme
+if (localStorage.getItem('prefered-theme') !== null) {
+    body.classList.replace('dark-theme', localStorage.getItem('prefered-theme'));
+} else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    body.classList.replace('dark-theme', 'light-theme')
+}
+
+// EventListener to change theme
+toggleTheme.addEventListener('click', () => {
+    if(body.classList.contains('dark-theme')) {
+        body.classList.replace('dark-theme', 'light-theme');
+        localStorage.setItem('prefered-theme', 'light-theme');
+    } else {
+        body.classList.replace('light-theme', 'dark-theme');
+        localStorage.setItem('prefered-theme', 'dark-theme');
+    }
+})
